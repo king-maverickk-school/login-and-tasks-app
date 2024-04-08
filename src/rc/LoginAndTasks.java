48a2591bad461ae72fd167c8c8ab59ff94f14759
king-maverickk-school <st10461408@rcconnect.edu.za>
2024-04-08 10:37:42 +0000

    initial commit

---
+++ src/rc/LoginAndTasks.java
@@ -0,0 +1,14 @@
+package rc;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginAndTasks {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
